64ef15fefd761e6c7451ee93b0976ba921dc9e94
testxxxxxxxxx <pppppmarcin@gmail.com>
2024-06-06 12:04:48 +0200

    removed test case

--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,6 @@ using namespace Graph;
 
 int main(int argc, char *argv[])
 {
-	//Test case:
-
-	/*GraphGenerator *graphGenerator = new GraphGenerator(7, 70);
-
-	//MatrixGraph *matrixGraph = graphGenerator->generateHamiltonian(); 
-
-	MatrixGraph *matrixGraph = graphGenerator->generateHamiltonian();
-
-	matrixGraph->print();
-
-	//Cycle *cycle = matrixGraph->findEulerCycle();
-
-	//cycle->print();
-
-	matrixGraph->findHamiltonianCycle();	
-
-	CommandLineEngine *cmd = new CommandLineEngine(matrixGraph);*/
-
 	if(!argv[1])
 	{
 		cout<<"Error"<<endl;
